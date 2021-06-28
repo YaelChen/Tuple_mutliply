a215fab19a0dee5d4a45eb50c3ebad8aee577695
Yael Chen <51058584+YaelChen@users.noreply.github.com>
2021-06-28 15:33:33 +0300

    Add files via upload

---
+++ 8.2.3 mult_tuple.py
@@ -0,0 +1,18 @@
+def mult_tuple(tuple1, tuple2):
+    """the function makes a tuple of all the possible pairs of numbers from the given tuples.
+    :param tuple1: a given tuple.
+    :param tuple2: a given tuple.
+    :type tuple1: tuple
+    :type tuple2: tuple
+    :return tuples_tuple: tuple of all the possible pairs of numbers from the given tuples.
+    :rtype tuples_tuple: tuple"""
+    tuples_list = []
+    for first in tuple1:
+        for second in tuple2:
+            tuples_list.append((first, second))
+            tuples_list.append((second, first))
+    tuples_tuple = tuple(tuples_list)
+    return tuples_tuple
+
+
+print(mult_tuple((1, 2, 3), (4, 5, 6)))
